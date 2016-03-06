2f401f68ef6b6074ca0b35163da0949a8b79e00f
Andrew Laptev <a.d.laptev@gmail.com>
2016-03-06 14:23:57 +0300

    Create lab0.fs

---
+++ lab0/lab0.fs
@@ -0,0 +1,29 @@
+module lab0
+
+open System
+open System.Net
+open System.Collections.Specialized
+
+let (email, name) = ("", "") // адрес почты и фамилия с инициалами
+
+let pascal c r = 1 // а тут решение
+
+let printIt n = 
+  "[" +
+  ([for x in 0..n do for y in 0..x do yield pascal y x] 
+    |> List.map (fun x -> x.ToString())
+    |> List.reduce (fun x y -> x + "," + y) )
+  + "]"
+
+let main () = 
+  let values = new NameValueCollection()
+  values.Add("email", email)
+  values.Add("name", name)
+  values.Add("lang", "fsharp")
+  values.Add("content", printIt 20)
+
+  let client = new WebClient()
+  let response = client.UploadValues(new Uri("http://91.239.143.158:13666/lab0"), values)
+  let responseString = Text.Encoding.Default.GetString(response)
+
+  printf "%A\n" responseString
