e29a26e4d64634b3e156069a63e5994f3ddcef3c
Andrew Laptev <a.d.laptev@gmail.com>
2016-03-06 14:25:23 +0300

    Create Lab0.hs

---
+++ lab0/Lab0.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad
+import Network.HTTP.Conduit
+import Data.Text.Encoding
+import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Char8 as C
+import Network (withSocketsDo)
+
+(email, name) = ("", encodeUtf8 "") -- адрес почты и фамилия с инициалами
+
+pascal :: Int -> Int -> Int
+pascal c r = 1 -- а тут решение
+
+printIt :: Int -> C.ByteString
+printIt n = C.pack $ show $ [pascal y x | x <- [0..n], y <- [0..x]]
+
+main :: IO()
+main = 
+  withSocketsDo $ do
+  initReq <- parseUrl "http://91.239.143.158:13666/lab0"
+  let req = urlEncodedBody [("email", email), ("name", name), ("lang", "haskell"), ("content", printIt 20)] $ initReq { method = "POST" }
+  response <- withManager $ httpLbs req
+  L.putStr $ responseBody response
